23b0c908002e5837cae12b7b6da00c5ee41b1f56
jck-bit <jckkinyanjui@gmail.com>
2023-03-15 17:30:40 +0300

    updated the index.html

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template,jsonify, request
+from flask import Flask, render_template,jsonify, request,redirect,url_for,flash
 import paypalrestsdk
+from paypalrestsdk import BillingPlan
+from datetime import datetime
 
 app = Flask(__name__)
-
 
 paypalrestsdk.configure({
     "mode":"sandbox",
@@ -14,41 +15,41 @@ paypalrestsdk.configure({
 def index():
     return render_template('index.html')
 
-@app.route('/payment', methods=['POST'])
-def payment():
+# @app.route('/payment', methods=['POST'])
+# def payment():
 
-    payment = paypalrestsdk.Payment({
-        "intent": "sale",
-        "payer": {
-            "payment_method": "paypal"},
-        "redirect_urls": {
-            "return_url": "http://localhost:5000/payment/execute",
-            "cancel_url": "http://localhost:5000/"},
-        "transactions": [{
-            "item_list": {
-                "items": [{
-                    "name": "testitem",
-                    "sku": "12345",
-                    "price": "500.00",
-                    "currency": "USD",
-                    "quantity": 1}]},
-            "amount": {
-                "total": "500.00",
-                "currency": "USD"},
-            "description": "This is the payment transaction description."}]})
+#     payment = paypalrestsdk.Payment({
+#         "intent": "sale",
+#         "payer": {
+#             "payment_method": "paypal"},
+#         "redirect_urls": {
+#             "return_url": "http://localhost:5000/payment/execute",
+#             "cancel_url": "http://localhost:5000/"},
+#         "transactions": [{
+#             "item_list": {
+#                 "items": [{
+#                     "name": "testitem",
+#                     "sku": "12345",
+#                     "price": "500.00",
+#                     "currency": "USD",
+#                     "quantity": 1}]},
+#             "amount": {-
+#                 "total": "500.00",
+#                 "currency": "USD"},
+#             "description": "This is the payment transaction description."}]})
 
-    if payment.create():
-        print('Payment success!')
-    else:
-        print(payment.error)
+#     if payment.create():
+#         print('Payment success!')
+#     else:
+#         print(payment.error)
 
-    return jsonify({'paymentID' : payment.id})
+#     return jsonify({'paymentID' : payment.id})
 
 
 @app.route('/execute', methods=['POST'])
 def execute():
 
-    payment = paypalrestsdk.Payment.find(request.form['paymentID'])
+    payment = paypalrestsdk.subs.find(request.form['paymentID'])
 
     if payment.execute({'payer_id': request.form['payerID']}):
         print('Execute success !')
@@ -57,6 +58,64 @@ def execute():
         print(payment.error)
     return jsonify({'success' : success})
 
+@app.route('/subscribe', methods=['POST'])
+def subscribe():
+    billing_plan = BillingPlan({
+    "name": "Fast Speed Plan",
+    "description": "Create Plan for Regular",
+    "merchant_preferences": {
+        "auto_bill_amount": "yes",
+        "cancel_url": "http://localhost:5000/subscribe",
+        "initial_fail_amount_action": "continue",
+        "max_fail_attempts": "1",
+        "return_url": "http://localhost:5000/",
+        "setup_fee": {
+            "currency": "USD",
+            "value": "25"
+        }
+    },
+    "payment_definitions": [
+        {
+            "amount": {
+                "currency": "USD",
+                "value": "100"
+            },
+            "charge_models": [
+                {
+                    "amount": {
+                        "currency": "USD",
+                        "value": "10.60"
+                    },
+                    "type": "SHIPPING"
+                },
+                {
+                    "amount": {
+                        "currency": "USD",
+                        "value": "20"
+                    },
+                    "type": "TAX"
+                }
+            ],
+            "cycles": "0",
+            "frequency": "MONTH",
+            "frequency_interval": "1",
+            "name": "Regular 1",
+            "type": "REGULAR"
+        }
+    ],
+    "type": "INFINITE"
+})
 
+    response = billing_plan.create()
+    print(response)
+
+
+    # Retrieve subscription ID from the response
+    subscription_id = response.id
+
+    # Return subscription ID as JSON response
+    print(subscription_id)
+    return {'subscription_id': subscription_id}
+    
 if __name__ == '__main__':
     app.run(debug=True)
